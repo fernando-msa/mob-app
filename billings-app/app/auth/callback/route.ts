a44cfe46a92df602ba40342721c6042c3d07e6b1
Fernando S. De Santana Júnior <ribeirojunior270@gmail.com>
2026-03-17 18:38:06 -0300

    fix-reverting-auth-callback-to-auth-helpers

--- billings-app/app/auth/callback/route.ts
+++ billings-app/app/auth/callback/route.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from '@supabase/ssr'
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
@@ -7,26 +7,10 @@ export async function GET(request: Request) {
   const code = requestUrl.searchParams.get('code')
 
   if (code) {
-    const cookieStore = cookies()
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          getAll() {
-            return cookieStore.getAll()
-          },
-          setAll(cookiesToSet) {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
-          },
-        },
-      }
-    )
+    const supabase = createRouteHandlerClient({ cookies })
     await supabase.auth.exchangeCodeForSession(code)
   }
 
-  // URL para onde o usuário será redirecionado após o login
+  // Redireciona o usuário para a página inicial após autenticar
   return NextResponse.redirect(requestUrl.origin)
 }
